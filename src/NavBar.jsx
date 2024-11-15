3d79b73d205fbdb4bffc461e935de4b287176558
hit7sh <hit7sh@gmail.com>
2024-11-15 19:19:42 +0530

    fix-gmail

--- src/NavBar.jsx
+++ src/NavBar.jsx
@@ -20,7 +20,7 @@ function Navbar({
             <Menu setActive={setActive}>
                 <MenuItem setActive={setActive} active={active} item="Contact Me">
                     <div className="flex flex-col space-y-4 text-sm">
-                        <HoveredLink href="https://mail.google.com/mail/u/0/#inbox?compose=GTvVlcSDZqhXrQXfVzFwtRvZrXWFKHpnhSrGCcPqkdWjtTszzZQtpVrLdwzfBHBvcccHlRjtPnmhV">G-Mail📩</HoveredLink>
+                        <HoveredLink href="https://mail.google.com/mail/?view=cm&fs=1&to=hit7sh@gmail.com">G-Mail📩</HoveredLink>
                         <HoveredLink href="https://wa.me/7725977528">WhatsApp📲</HoveredLink>
                         <HoveredLink href="https://www.linkedin.com/in/hitesh-saini-80499a1b3/">LinkedIn</HoveredLink>
                         <HoveredLink href="https://x.com/hit7sh">Twitter</HoveredLink>
